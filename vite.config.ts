6157a6b50e1a066bde93e682d449a6ac015466ef
Samvel <kuznecovartemij09@gmail.com>
2024-07-26 18:11:52 +0500

    add_test_commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/nutritiolntime/',
+	base: '/NutritionTime_Dev-test/',
 	server: {
 		port: 9090
 	},
