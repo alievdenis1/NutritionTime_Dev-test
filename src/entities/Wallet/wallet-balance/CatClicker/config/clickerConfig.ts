90dd2d24d21051172b5a88683eed7f6ecff1fd1b
d.ryzhkov <d.ryzhkov@cpfintech.ru>
2024-10-18 14:20:31 +0300

    update sound threshold

--- src/entities/Wallet/wallet-balance/CatClicker/config/clickerConfig.ts
+++ src/entities/Wallet/wallet-balance/CatClicker/config/clickerConfig.ts
@@ -50,7 +50,7 @@ export const CLICKER_CONFIG = reactive({
         rapidClickDuration: 85     // Длительность вибрации при быстром клике
     },
     sound: {
-        thresholdLow: 2.75,
+        thresholdLow: 3.00,
         thresholdMedium: 3.75,
         thresholdHigh: 3.95,
         timeout: 1000,
